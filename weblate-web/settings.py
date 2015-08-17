1d621de3712ac73eb45985dacf1657902e78ee36
Michal Čihař <michal@cihar.com>
2015-08-17 19:00:31 +0200

    Use MySQL module provided variables
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate-web/settings.py
+++ weblate-web/settings.py
@@ -56,15 +56,15 @@ DATABASES = {
         # Use 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'ENGINE': 'django.db.backends.mysql',
         # Database name or path to database file if using sqlite3.
-        'NAME': os.environ['MYSQL_NAME'],
+        'NAME': os.environ['WEBLATE_DB_ENV_MYSQL_DATABASE'],
         # Database user, not used with sqlite3.
-        'USER': os.environ['MYSQL_USER'],
+        'USER': os.environ['WEBLATE_DB_ENV_MYSQL_USER'],
         # Database password, not used with sqlite3.
-        'PASSWORD': os.environ['MYSQL_PASSWORD'],
+        'PASSWORD': os.environ['WEBLATE_DB_ENV_MYSQL_PASSWORD'],
         # Set to empty string for localhost. Not used with sqlite3.
-        'HOST': os.environ['MYSQL_HOST'],
+        'HOST': os.environ['WEBLATE_DB_PORT_3306_TCP_ADDR'],
         # Set to empty string for default. Not used with sqlite3.
-        'PORT': os.environ['MYSQL_PORT'],
+        'PORT': '3306',
     }
 }
 
