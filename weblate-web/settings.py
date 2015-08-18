23496ccc7ad7116328f4b8615bd0fc2faae81965
Michal Čihař <michal@cihar.com>
2015-08-18 07:30:48 +0200

    Allow to switch database backends
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- weblate-web/settings.py
+++ weblate-web/settings.py
@@ -45,8 +45,10 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
+DATABASES = {}
+
+if 'WEBLATE_DB_ENV_MYSQL_DATABASE' in os.environ:
+    DATABASES['default'] = {
         # Use 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'ENGINE': 'django.db.backends.mysql',
         # Database name or path to database file if using sqlite3.
@@ -63,7 +65,39 @@ DATABASES = {
         # Set to empty string for default. Not used with sqlite3.
         'PORT': os.environ['WEBLATE_DB_PORT_3306_TCP_PORT'],
     }
-}
+elif 'WEBLATE_DB_ENV_POSTGRES_USER' in os.environ:
+    DATABASES['default'] = {
+        # Use 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # Database name or path to database file if using sqlite3.
+        'NAME': os.environ['WEBLATE_DB_ENV_POSTGRES_USER'],
+        # Use same database for tests (needed as Docker MySQL can
+        # not currently create second database for us)
+        'TEST_NAME': os.environ['WEBLATE_DB_ENV_POSTGRES_USER'],
+        # Database user, not used with sqlite3.
+        'USER': os.environ['WEBLATE_DB_ENV_POSTGRES_USER'],
+        # Database password, not used with sqlite3.
+        'PASSWORD': os.environ['WEBLATE_DB_ENV_POSTGRES_PASSWORD'],
+        # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': os.environ['WEBLATE_DB_PORT_5432_TCP_ADDR'],
+        # Set to empty string for default. Not used with sqlite3.
+        'PORT': os.environ['WEBLATE_DB__PORT_5432_TCP_PORT'],
+    }
+else:
+    DATABASES['default'] = {
+        # Use 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.sqlite3',
+        # Database name or path to database file if using sqlite3.
+        'NAME': 'weblate.db',
+        # Database user, not used with sqlite3.
+        'USER': 'weblate',
+        # Database password, not used with sqlite3.
+        'PASSWORD': 'weblate',
+        # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '127.0.0.1',
+        # Set to empty string for default. Not used with sqlite3.
+        'PORT': '',
+    }
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
