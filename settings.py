3269e363721cf09ab5ec5baeab770adea7c30ac7
Michal Čihař <michal@cihar.com>
2017-04-12 19:59:54 +0200

    Default to UTC timezone
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- settings.py
+++ settings.py
@@ -68,7 +68,7 @@ DATA_DIR = '/app/data'
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = os.environ.get('WEBLATE_TIME_ZONE', None)
+TIME_ZONE = os.environ.get('WEBLATE_TIME_ZONE', 'UTC')
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
