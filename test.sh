a1279a671dec28bd1eeb5c67c81a091cc37ad6ba
Michal Čihař <michal@cihar.com>
2017-08-03 22:54:21 +0200

    Run tests verbosely
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- test.sh
+++ test.sh
@@ -9,9 +9,9 @@ CONTAINER=`docker-compose ps | grep _weblate_ | sed 's/[[:space:]].*//'`
 IP=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONTAINER`
 echo "Checking '$CONTAINER', IP address '$IP'"
 TIMEOUT=0; while ! curl --fail --silent --output /dev/null "http://$IP/" ; do sleep 1 ; TIMEOUT=$(($TIMEOUT + 1)); if [ $TIMEOUT -gt 60 ] ; then break ;fi ; done
-curl --fail "http://$IP/about/" | grep 'Powered by.*Weblate'
+curl --verbose --fail "http://$IP/about/" | grep 'Powered by.*Weblate'
 RET=$?
-curl --fail --output /dev/null "http://$IP/static/weblate-128.png"
+curl --verbose --fail --output /dev/null "http://$IP/static/weblate-128.png"
 if [ $? -ne 0 -o $RET -ne 0 ] ; then
     docker-compose logs
     docker exec $CONTAINER tail /var/log/syslog
