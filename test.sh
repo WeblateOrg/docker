b07091f6058355a25254b222336326c483462c42
Michal Čihař <michal@cihar.com>
2018-05-18 08:43:29 +0200

    These logs are gone
    See https://github.com/WeblateOrg/docker/pull/81
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- test.sh
+++ test.sh
@@ -29,8 +29,6 @@ curl --verbose --fail --output /dev/null "http://$IP/static/weblate-128.png"
 if [ $? -ne 0 -o $RET -ne 0 ] ; then
     docker-compose logs
     docker exec $CONTAINER tail /var/log/syslog
-    docker exec $CONTAINER tail /var/log/nginx/error.log
-    docker exec $CONTAINER tail /var/log/uwsgi/app/weblate.log
     exit 1
 fi
 
