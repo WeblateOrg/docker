2d147d25c1b745d29b69c7c4014d1d852a326b2c
Marek Suscak <marek.suscak@gmail.com>
2019-06-15 01:22:02 +0200

    Make /healthz/ endopint exempt from SSL redirect

--- etc/weblate/settings.py
+++ etc/weblate/settings.py
@@ -697,6 +697,10 @@ CSRF_USE_SESSIONS = True
 SESSION_COOKIE_SECURE = ENABLE_HTTPS
 # SSL redirect
 SECURE_SSL_REDIRECT = ENABLE_HTTPS
+# SSL redirect URL exemption list
+SECURE_REDIRECT_EXEMPT = (
+   r'/healthz/$',           # Allowing HTTP access to health check
+)
 # Session cookie age (in seconds)
 SESSION_COOKIE_AGE = 1209600
 
