c0ea897bcdcdc9a20dc0dd919fc2115e570d751d
sarnouts <sylvain.arnouts@soprasteria.com>
2019-07-10 13:56:36 +0200

    feat(port): add an env var for the web server port

--- etc/nginx/run.sh
+++ etc/nginx/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -e "s%{{URL_PREFIX}}%${WEBLATE_URL_PREFIX}%g" /etc/nginx/default.tpl > /etc/nginx/sites-available/default
+envsubst < /etc/nginx/default.tpl > /etc/nginx/sites-available/default;
 exec /usr/sbin/nginx -g "daemon off;"
