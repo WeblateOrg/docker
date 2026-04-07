3c23a2cd0d0c1b4dcdbe5c19c0a9249dca82520e
Michal Čihař <michal@cihar.com>
2026-04-07 10:24:25 +0200

    feat: listen on ipv6 sockets as well
    Fixes #4391

--- etc/nginx/generate-site.py
+++ etc/nginx/generate-site.py
@@ -14,6 +14,7 @@ from django.conf import settings
     SITE_DOMAIN,
     ENABLE_HTTPS,
     GRANIAN_SOCKET,
+    ENABLE_IPV6,
 ) = sys.argv[1:]
 
 WEBLATE_SITE_URL = "{}://{}".format(
@@ -47,6 +48,7 @@ print(
             "WEBLATE_ANUBIS_URL": WEBLATE_ANUBIS_URL,
             "WEBLATE_SITE_URL": WEBLATE_SITE_URL,
             "GRANIAN_SOCKET": GRANIAN_SOCKET,
+            "ENABLE_IPV6": ENABLE_IPV6,
         }
     )
 )
